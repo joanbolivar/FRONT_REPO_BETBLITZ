f994bd817851158beb50bff2da8fb81481f16ff1
Joan Manuel Bolivar Agudelo <joboliva@bancolombia.com.co>
2024-04-30 21:06:12 -0500

    Revert

--- betblitz-app/src/app/app.routes.ts
+++ betblitz-app/src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/components/home.component';
 import { ContactComponent } from './modules/contact/components/contact.component';
-import { UserBetsComponent } from './modules/userBets/components/user-bets.component';
-import { AdminBetsComponent } from './modules/adminBets/components/admin-bets.component';
 
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path:'contact', component: ContactComponent},
-    {path:'user-bets', component: UserBetsComponent},
-    {path:'admin-bets', component: AdminBetsComponent},
+    {path:'contact', component: ContactComponent}
 ];
